41a5eb045b5c8ee03f4f1ca2b300f6ddbde5f109
Annas Alghoffar <aag.annas@gmail.com>
2022-04-24 01:37:13 +0700

    feat: Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./Button.scss";
+import PropTypes from "prop-types";
+import clsx from "clsx";
 
-// eslint-disable-next-line react/prop-types
-const Button = ({ children, ...props }) => {
+const Button = ({ children, variant = "filled", className, ...props }) => {
   return (
-    <button className="button" {...props}>
+    <button
+      className={clsx(
+        "button",
+        {
+          "button-filled": variant === "filled",
+          "button-outlined": variant === "outlined",
+          "button-text": variant === "text",
+          "button-icon": variant === "icon",
+        },
+        className
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  variant: PropTypes.oneOf(["filled", "outlined", "text", "icon"]),
+  className: PropTypes.string,
 };
 
 export default Button;
