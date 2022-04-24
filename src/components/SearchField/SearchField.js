5ec8ecd1f21220a0941bb798059f3a880396a59d
Annas Alghoffar <aag.annas@gmail.com>
2022-04-24 07:43:05 +0700

    refactor: add breakpoint on search field

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -2,12 +2,17 @@ import React from "react";
 import Flex from "../Flex";
 import { MdOutlineSearch } from "react-icons/md";
 import "./SearchField.scss";
+import useBreakpoint from "use-breakpoint";
+import { BREAKPOINTS } from "../../constants/breakpoints";
 
 const SearchField = () => {
+  const { breakpoint } = useBreakpoint(BREAKPOINTS);
   return (
     <Flex container justifyContent="center" alignItems="center">
       <MdOutlineSearch className="search-icon" />
-      <input type="text" placeholder="Search" className="search-field" />
+      {breakpoint !== "xs" && (
+        <input type="text" placeholder="Search" className="search-field" />
+      )}
     </Flex>
   );
 };
