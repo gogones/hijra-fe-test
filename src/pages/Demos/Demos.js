f67efb4483d54455ca739974459ad9566dc4b3d1
Annas Alghoffar <aag.annas@gmail.com>
2022-04-24 07:45:29 +0700

    refactor: Demos page component

--- src/pages/Demos/Demos.js
+++ src/pages/Demos/Demos.js
@@ -5,27 +5,24 @@ import Portofolio from "../../components/Portofolio";
 import Button from "../../components/Button";
 import { FiChevronRight } from "react-icons/fi";
 import Flex from "../../components/Flex";
+import BannerText from "../../components/BannerText";
 
 const Demos = () => {
   return (
-    <div>
-      {/* Navbar */}
+    <div className="app-content">
       <Navbar />
 
       {/* Content */}
       <div className="page-content">
-        <div className="banner-hero">
-          <div className="banner-content-container">
-            <div className="title-banner">Catering should be an experience</div>
-
-            <div className="title-standout-banner">
-              We use only the finest and freshest ingredients
-            </div>
-
-            <div className="title-detail-banner">
-              At sway catering we know that food is an important part of life.
-              If the meal is not perfect, your event cannot be perfect.
-            </div>
+        <div className="banner">
+          <div className="content">
+            <BannerText
+              rootClassName="banner-text"
+              title="Catering should be an experience"
+              subtitle="We use only the finest and freshest ingredients"
+              detail="At sway catering we know that food is an important part of life.
+                If the meal is not perfect, your event cannot be perfect."
+            />
 
             <Button>
               <Flex container justifyContent="center" alignItems="center">
