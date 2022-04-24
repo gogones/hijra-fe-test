61d567070c2f2fbb93da3ec8939b0b2b7129f039
Annas Alghoffar <aag.annas@gmail.com>
2022-04-24 07:42:21 +0700

    refactor: add forwardRef to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,25 +3,29 @@ import "./Button.scss";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
-const Button = ({ children, variant = "filled", className, ...props }) => {
-  return (
-    <button
-      className={clsx(
-        "button",
-        {
-          "button-filled": variant === "filled",
-          "button-outlined": variant === "outlined",
-          "button-text": variant === "text",
-          "button-icon": variant === "icon",
-        },
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+// eslint-disable-next-line react/display-name
+const Button = React.forwardRef(
+  ({ children, variant = "filled", className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={clsx(
+          "button",
+          {
+            "button-filled": variant === "filled",
+            "button-outlined": variant === "outlined",
+            "button-text": variant === "text",
+            "button-icon": variant === "icon",
+          },
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 Button.propTypes = {
   children: PropTypes.node,
